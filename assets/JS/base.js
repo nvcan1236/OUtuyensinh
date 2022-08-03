d6a346dbd690832dd1c5441df9dce0ba3ecc8b41
NguyenVanCanh <ngcanh1236@gmail.com>
2022-08-03 15:37:51 +0700

    upload

---
+++ assets/JS/base.js
@@ -0,0 +1,65 @@
+window.addEventListener("load", function () {
+  // slider
+  const slider = document.querySelector(".slider");
+  const sliderMain = document.querySelector(".slider-main");
+  const sliderItems = document.querySelectorAll(".slider-item");
+  const prevBtn = document.querySelector(".slider-prev");
+  const nextBtn = document.querySelector(".slider-next");
+  const dot = document.querySelectorAll(".slider-dot");
+  var slideIndex = 0;
+  const slideItemWidth = sliderItems[0].offsetWidth;
+  const slideLength = sliderItems.length;
+  var posX = 0;
+  
+  nextBtn.addEventListener("click", function () {
+    changeSlide(1);
+  });
+  
+  prevBtn.addEventListener("click", function () {
+    changeSlide(-1);
+  });
+  autoNextSlide();
+  
+  function changeSlide(direction) {
+    if (direction === 1) {
+      if (slideIndex === slideLength - 1) {
+        posX = 0;
+        slideIndex = 0;
+        sliderMain.style = `transform: translateX(${posX}px)`;
+        return;
+      }
+      slideIndex++;
+      posX = posX - slideItemWidth;
+      sliderMain.style = `transform: translateX(${posX}px)`;
+    } else if (direction === -1) {
+      if (slideIndex === 0) {
+        slideIndex = slideLength - 1;
+        posX = -slideItemWidth * (slideLength - 1);
+        sliderMain.style = `transform: translateX(${posX}px)`;
+        return;
+      }
+      slideIndex--;
+      posX = posX + slideItemWidth;
+      sliderMain.style = `transform: translateX(${posX}px)`;
+    }
+  }
+  
+  function autoNextSlide() {
+    setInterval(function () {
+      changeSlide(1);
+    }, 5000);
+  }
+  
+  // nav - tool
+  const navItems = document.querySelectorAll(".nav-tool-item");
+  console.log(navItems);
+
+  for(var i=0; i < navItems.length; i++) {
+      navItems[i].onclick = function() {
+          // navItems[i].classList.remove('current');
+          // this.classList.add("current");
+        console.log(navItems);
+        alert("a");
+      };
+  }
+});
