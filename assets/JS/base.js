d631a848c7ed1f14d86f930f141a5b22478a0075
NguyenVanCanh <ngcanh1236@gmail.com>
2022-08-07 21:43:42 +0700

    ca-8/7

--- assets/JS/base.js
+++ assets/JS/base.js
@@ -84,8 +84,25 @@ window.addEventListener("load", function () {
     topFunction();
   }
 
+  // window.onscroll = function() {
+  // }
+
+  // header 
+  var header = document.querySelector("header");
+  var headerLogo = document.querySelector(".header__logo");
+  function scrollFunction2() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.classList.add("fixed");
+      headerLogo.style = "display: none;"
+    }
+    else {
+      header.classList.remove("fixed");
+      headerLogo.style = "display: block;"
+    }
+  }
   window.onscroll = function() {
     scrollFunction();
+    scrollFunction2();
   }
 });
 
